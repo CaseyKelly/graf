61451d009ac639b7948c3771c24e8bce5261be1a
CaseyKelly <casey.kelly@colorado.edu>
2015-06-03 14:12:57 -0400

    add posts migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150603181132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "headline"
+    t.string "subhead"
+    t.string "byline"
+    t.text   "main_body"
+  end
 
 end
