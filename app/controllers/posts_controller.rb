b73aa45a71cfeeb30f831492fa0e6c955c355cd7
CaseyKelly <casey.kelly@colorado.edu>
2015-06-06 20:59:04 -0400

    add create action to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to post_path(@post), notice: 'Post was successfully created.'
+      else
+        render :index
+      end
+  end
+
+  def post_params
+    params.require(:post).permit(:headline, :subhead, :byline, :text_body)
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,13 @@ class WelcomeController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to post_path(@post), notice: 'Post was successfully created.'
+      else
+        render :new
+      end
+  end
+
 end
