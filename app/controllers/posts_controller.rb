dfffa27e9b97fbf4cf8534b8650f1c61ea601c58
CaseyKelly <casey.kelly@colorado.edu>
2015-06-08 10:22:34 -0600

    style rich text input box and merge buttons on main page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:headline, :subhead, :byline, :text_body)
+    params.require(:post).permit(:headline, :subhead, :byline, :main_body)
   end
 
 end
